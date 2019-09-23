521d45ec1af278044f40ca6f7e51bee2cc9c7b66
Charles-Basile <cbrdev28@gmail.com>
2019-09-22 17:17:40 -0700

    Make rubocop green

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# My first class to show some json
 class WelcomeController < ApplicationController
   def show
     @title = 'Welcome CBR controller'
--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,13 @@ require 'action_cable/engine'
 Bundler.require(*Rails.groups)
 
 module RbContreeServer
+  # The base application class from Rails api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over
+    # those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
