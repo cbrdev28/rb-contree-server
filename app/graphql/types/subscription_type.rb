a07085d8578b1951a3803c74fd66a0e01400897c
Charles-Basile <cbrdev28@gmail.com>
2020-12-20 13:27:04 -0800

    Add first test subscription

--- app/graphql/types/subscription_type.rb
+++ app/graphql/types/subscription_type.rb
@@ -6,10 +6,12 @@
 # class Types::SubscriptionType < GraphQL::Schema::Object
 # end
 
-# Define the GraphQL type for subscription, same as the example above
-# but this uses the same pattern than our mutation and query base types
 module Types
+  # Define the GraphQL type for subscription, same as the example above
+  # but this uses the same pattern than our mutation and query base types
   class SubscriptionType < Types::BaseObject
     # Declare subscriptions here, like we do with mutations
+
+    field :test_messages, subscription: Subscriptions::TestMessages
   end
 end
