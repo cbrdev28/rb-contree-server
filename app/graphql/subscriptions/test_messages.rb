27b64ff2c41335e871784b78c6bf8961d1ca32e9
Charles-Basile <cbrdev28@gmail.com>
2020-12-20 14:40:03 -0800

    Fix usage of Rails.logger by doing a warn instead of log

--- app/graphql/subscriptions/test_messages.rb
+++ app/graphql/subscriptions/test_messages.rb
@@ -8,7 +8,7 @@ module Subscriptions
     field :messages, [String], null: false
 
     def subscribe(test_id: nil)
-      Rails.logger.log "TestMessages Subscription: subscribe: #{test_id}"
+      Rails.logger.warn "TestMessages Subscription: subscribe #{test_id}"
 
       # Return initial value
       {
@@ -17,7 +17,7 @@ module Subscriptions
     end
 
     def update(test_id: nil)
-      Rails.logger.log "TestMessages Subscription: update: #{test_id}"
+      Rails.logger.warn 'TestMessages Subscription: update'
       # Return some debug value
       {
         messages: ['One value from update']
